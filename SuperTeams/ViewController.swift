e6a08321ac08fb788b544b5a528c8e432c89da44
Evgeny Pesterev <apsterio.anima@gmail.com>
2021-11-11 22:00:52 +0500

    Initial Commit

---
+++ SuperTeams/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SuperTeams
+//
+//  Created by evpes on 11.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
